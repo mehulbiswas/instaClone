bb15a44ad8e0e1908a94b2356f4d42ee66a9c2bd
Mehul <59968527+mehulbiswas@users.noreply.github.com>
2020-02-29 16:05:33 +0530

    uploaded files

---
+++ instaClone/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from myapp.views import signup_view , login_view , feed_view , post_view, like_view, comment_view, logout_view
+
+urlpatterns = [
+    url('/login/', logout_view),
+    url('post/', post_view),
+    url('feed/',feed_view),
+    url('like/', like_view),
+    url('comment/', comment_view),
+    url('login/',login_view),
+    url('', signup_view),
+
+
+]
